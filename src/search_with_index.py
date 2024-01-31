ea97088a912a1dc68a57615ca161a499b2c01ea0
Varun Medappa <varunfer@gmail.com>
2024-01-31 03:57:01 +0000

    Searches the index for text relevant to the query

---
+++ src/search_with_index.py
@@ -0,0 +1,25 @@
+from autofaiss_index import AutoFaissSentenceSearch
+import configparser
+
+def load_index_and_search(query, sentence_model,index_folder,max_index_memory_usage):
+    afss = AutoFaissSentenceSearch(sentence_model=sentence_model, index_folder=index_folder, max_index_memory_usage=max_index_memory_usage)
+    afss.load_index()
+    afss.load_dataframe() 
+    search_results = afss.search_sentences(query)
+    return search_results
+
+if __name__ == "__main__":
+
+    # Read configuration
+    config = configparser.ConfigParser()
+    config.read('config.txt')
+    
+    query = config['DEFAULT']['query']
+    model = config['DEFAULT']['model']
+    index_folder = config['DEFAULT']['index_folder']
+    max_index_memory_usage = config['DEFAULT'].get('max_index_memory_usage', '10MB')
+
+    
+    results = load_index_and_search(query, model, index_folder, max_index_memory_usage)
+
+    print(results)
