1eb0ab1d06ccbaf7c58c41fe6f22c30c3f8b8c7f
Harsha R <97391745+genkai10@users.noreply.github.com>
2024-03-30 10:25:12 +0530

    Add files via upload

---
+++ src/CrossEncoderSearch.py
@@ -0,0 +1,25 @@
+import os
+import re
+from sentence_transformers import CrossEncoder
+from sentence_transformers import SentenceTransformer
+
+class CrossencoderSearch:
+
+    def __init__(self,query,retrieved_docs,run_cross_encoding=False):
+
+        self.query=query
+        self.pairs=retrieved_docs
+        self.final_pairs=[]
+        if run_cross_encoding:
+            self.cross_encoding_model= CrossEncoder('BAAI/bge-base-en-v1.5')
+
+    def run_cross_encoder(self):
+        self.final_pairs = [[self.query, doc_text, self.cross_encoding_model.predict([self.query, doc_text])] for doc_text in self.pairs]
+        self.final_pairs.sort(key=lambda x: x[2])
+        self.final_pairs.reverse()
+        return self.final_pairs
+
+        
+        
+        
+
