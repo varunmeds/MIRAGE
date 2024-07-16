b7e7ffce020078d805e3918f237c3395512056d4
Harsha R <97391745+genkai10@users.noreply.github.com>
2024-07-16 19:02:37 +0530

    Update search_with_index.py

--- src/search_with_index.py
+++ src/search_with_index.py
@@ -1,6 +1,7 @@
 from autofaiss_index import AutoFaissSentenceSearch
 from CrossEncoderSearch import CrossencoderSearch, dict_to_list
 import configparser
+import time
 
 def load_index_and_search(query, sentence_model,index_folder,max_index_memory_usage):
     afss = AutoFaissSentenceSearch(sentence_model=sentence_model, index_folder=index_folder, max_index_memory_usage=max_index_memory_usage)
@@ -20,12 +21,18 @@ if __name__ == "__main__":
     index_folder = config['DEFAULT']['index_folder']
     max_index_memory_usage = config['DEFAULT'].get('max_index_memory_usage', '10MB')
     
+    start_time = time.time()
     results = load_index_and_search(query, model, index_folder, max_index_memory_usage)
+    end_time = time.time()
+
+    time_taken = end_time - start_time
     input=dict_to_list(results)
 
     if(config['DEFAULT']['cross_encoder_rerank']):
         ce = CrossencoderSearch(query,input)
         outputs = ce.run_cross_encoder()
         print(outputs)
+        print(time_taken)
     else:
         print(results)
+        print(time_taken)
