b9dd3090a48bc89998c165e24dd5d7758c3bc9e1
Harsha R <97391745+genkai10@users.noreply.github.com>
2024-09-18 21:59:48 +0530

    Update search_with_index.py

--- src/search_with_index.py
+++ src/search_with_index.py
@@ -20,18 +20,27 @@ def load_index_and_test(query, sentence_model, index_folder, max_index_memory_us
     test_file_path = os.path.join(test_dir,query)
     with open(test_file_path, 'r') as file:
         loaded_text_list = json.load(file)
-    random.shuffle(loaded_text_list)
-    split_index = int(len(loaded_text_list) * 0.2853)
+    #random.shuffle(loaded_text_list)
+    '''split_index = int(len(loaded_text_list) * 0.2853)
     texts_query = loaded_text_list[:split_index]
     #texts_list = texts[split_index:]
     texts_query_split_index = int(len(texts_query) * 0.2)
     texts_query_small = texts_query[:texts_query_split_index]
-    texts_list_small = texts_query[texts_query_split_index:]
+    texts_list_small = texts_query[texts_query_split_index:]'''
+    subset_list = loaded_text_list[:20000]
+    texts_query_small = subset_list[:2000]
+    texts_list_small = subset_list[2000:20000]
+    print("Start of function")
     df = afss.create_dataframe_from_texts(texts_list_small, default_filename=test_file_path)
+    print("database created")
     afss.save_dataframe_from_texts(df, filename="test_dataframe.pkl")
+    print("database saved")
     afss.build_index_from_texts()
+    print("index built for database")
     afss.load_index()
+    print("index loaded")
     search_results = afss.search_sentences_test(texts_query_small)
+    print("End of function")
     return search_results
 
 if __name__ == "__main__":
@@ -43,9 +52,6 @@ if __name__ == "__main__":
     query = config['DEFAULT']['query']
     #query = config['DEFAULT']['query'].split(',')
     test_flag = config['DEFAULT']['test_flag']
-    print(type(query))
-    print(query)
-    print(test_flag)
     model = config['DEFAULT']['model']
     index_folder = config['DEFAULT']['index_folder']
     max_index_memory_usage = config['DEFAULT'].get('max_index_memory_usage', '10MB')
@@ -59,9 +65,9 @@ if __name__ == "__main__":
         results = load_index_and_search(query, model, index_folder, max_index_memory_usage,matryoshka,matryoshka_dim)
     #input=dict_to_list(results)
     print(results)
-    file_name = 'query_times_64.txt'
+    '''file_name = 'query_times_64.txt'
     with open(file_name, 'w') as file:
-        json.dump(results, file)
+        json.dump(results, file)'''
 
     '''if(config['DEFAULT']['cross_encoder_rerank']):
         ce = CrossencoderSearch(query,input)
